8ac7d27d03f69663a4c73fb4632a81647af4ca0f
Minsoo Kim <holiday1883@gmail.com>
2023-02-19 15:55:51 +0900

    add file

---
+++ ALGORITHMS/javatest/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        int abc= a.nextInt();
+        int abcd= a.nextInt();
+
+        System.out.println(abc+abcd);
+    }
+}
---
+++ ALGORITHMS/untitled/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Sacnner a = new Scanner(System.in);
+        Integer b = a.nextLine();
+        Integer c = a.nextLine();
+        Integer d = b+c;
+        System.out.println(d);
+    }
+}
